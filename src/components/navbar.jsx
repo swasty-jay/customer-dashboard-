a3cc44a48f5371d6046dfc8e92a3c590fadf8c83
swasty jay <amekpoagbedaniel@gmail.com>
2024-10-20 09:21:50 +0000

    adding more stuffs

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,43 @@
 // src/components/Navbar.jsx
 import React from "react";
-import { Container, Navbar, Form, FormControl } from "react-bootstrap";
-
-// import "./app.css";
+import { Navbar, Container, Form, FormControl } from "react-bootstrap";
+import { IoPersonCircleSharp, IoNotificationsSharp } from "react-icons/io5"; // Importing Ionicons icons
 
 const MyNavbar = () => {
   return (
     <>
       <Navbar bg="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#" className="text-light ">
+          <IoPersonCircleSharp size={60} color="white" className="mb-1  " />
+          <div className="vertical-line">
+            <hr />
+          </div>
+          <Navbar.Brand href="#" className="text-light">
             ORGANIZED
+            <div style={{ fontSize: "0.7rem", fontWeight: "lighter" }}>
+              swasty jay
+            </div>{" "}
+            {/* Username */}
           </Navbar.Brand>
-          <Form className="d-flex col-sm-1 me-auto ">
+          <Form className="d-flex col-sm-1 me-auto">
             <FormControl
               type="search"
               placeholder="Search..."
               className="me-1"
             />
           </Form>
-          {/* User Avatar */}
-          <img
-            src="https://via.placeholder.com/30" // Replace with actual avatar URL
-            alt="User Avatar"
-            className="rounded-circle"
-            width="30"
-            height="30"
-          />
+
+          {/* User menu with notification bell and avatar icon */}
+          <div className="user-menu ms-3 text-light d-flex align-items-center">
+            {/* Notification Bell Icon */}
+            <IoNotificationsSharp size={30} className="me-2" color="white" />
+
+            {/* IoPersonCircleSharp Icon from react-icons */}
+            <IoPersonCircleSharp size={40} color="white" />
+          </div>
         </Container>
       </Navbar>
       {/* SECOND NAVBAR */}
-
       <nav class="navbar navbar-expand-lg bg-body-tertiary mb-3">
         <div class="container-fluid py-0">
           <a class="navbar-brand" href="#">
