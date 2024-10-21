fc7d4e28745b3432a2f6ac8cf42208fd9692b7ba
swasty jay <amekpoagbedaniel@gmail.com>
2024-10-21 17:35:37 +0000

    adding more styling

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./app.css";
+// import "./app.css";
 // import "./sidebar.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
